e74211a9f7dd14d5a03bca0976684d45a9a63f7f
Mika-lintu <mikalintula@kamk.fi>
2017-05-12 17:25:35 +0300

    Update HealthUI.cs

--- Assets/UI/HealthUI.cs
+++ Assets/UI/HealthUI.cs
@@ -18,24 +18,26 @@ public class HealthUI : MonoBehaviour {
     }
 	
 	void Update () {
-
+//Updates HealthBars position if object it is attached moves and offset set it so it's on top of object 
         if (active)
         {
             transform.position = Camera.main.WorldToScreenPoint((Vector3.up * offset) + target.transform.position);
         }
 	}
-
+//Updates health image to show correct amount of color
     public void MinusHealth(float damage)
     {
        hp.fillAmount = (float)cube.health / (float)cube.startHealth;
-        Debug.Log(cube.startHealth + " " + cube.health + " " +  hp.fillAmount);
        GetEmptyText(damage);
     }
+//Sets HealthBar disabled 
     public void DisableHealthBar()
     {
         active = false;
         gameObject.SetActive(false);
     }
+//Finds empty text object form list and activates it
+//Then it gives an amount how much damage was
     public void GetEmptyText(float dmg)
     {
         for (int i = 0; i < textList.Count; i++)
